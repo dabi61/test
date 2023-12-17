7c1bebd7eb4e3324de1494c8a2b7c436aa9749a2
Mons with Friend <daodinhbinh612004@gmail.com>
2023-12-17 20:42:12 +0700

    test upload

---
+++ SqlAdapter/SqlAdapter/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SqlAdapter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gvSanPham = new System.Windows.Forms.DataGridView();
+            this.btnNhapDuLieu = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtGia = new System.Windows.Forms.TextBox();
+            this.txtMadm = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvSanPham)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gvSanPham
+            // 
+            this.gvSanPham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvSanPham.Location = new System.Drawing.Point(58, 88);
+            this.gvSanPham.Name = "gvSanPham";
+            this.gvSanPham.Size = new System.Drawing.Size(325, 240);
+            this.gvSanPham.TabIndex = 0;
+            this.gvSanPham.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvSanPham_CellClick);
+            // 
+            // btnNhapDuLieu
+            // 
+            this.btnNhapDuLieu.Location = new System.Drawing.Point(58, 42);
+            this.btnNhapDuLieu.Name = "btnNhapDuLieu";
+            this.btnNhapDuLieu.Size = new System.Drawing.Size(314, 24);
+            this.btnNhapDuLieu.TabIndex = 1;
+            this.btnNhapDuLieu.Text = "Nhap Du Lieu";
+            this.btnNhapDuLieu.UseVisualStyleBackColor = true;
+            this.btnNhapDuLieu.Click += new System.EventHandler(this.btnNhapDuLieu_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mã";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(444, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(444, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Gía";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Mã Danh Mục";
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(502, 88);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(208, 20);
+            this.txtMa.TabIndex = 6;
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(502, 125);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(208, 20);
+            this.txtTen.TabIndex = 7;
+            // 
+            // txtGia
+            // 
+            this.txtGia.Location = new System.Drawing.Point(502, 169);
+            this.txtGia.Name = "txtGia";
+            this.txtGia.Size = new System.Drawing.Size(208, 20);
+            this.txtGia.TabIndex = 8;
+            // 
+            // txtMadm
+            // 
+            this.txtMadm.Location = new System.Drawing.Point(502, 214);
+            this.txtMadm.Name = "txtMadm";
+            this.txtMadm.Size = new System.Drawing.Size(208, 20);
+            this.txtMadm.TabIndex = 9;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(502, 263);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 10;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(624, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Sua";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 358);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtMadm);
+            this.Controls.Add(this.txtGia);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNhapDuLieu);
+            this.Controls.Add(this.gvSanPham);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gvSanPham)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gvSanPham;
+        private System.Windows.Forms.Button btnNhapDuLieu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtGia;
+        private System.Windows.Forms.TextBox txtMadm;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SqlAdapter/SqlAdapter/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SqlAdapter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = null;
+        string strConn = "Server = DESKTOP-IKHI39U\\MONS;Database = CSDLTest; User Id = sa; pwd = Entercc12";
+        SqlDataAdapter adapter = null;
+        DataSet ds = null;
+        private void btnNhapDuLieu_Click(object sender, EventArgs e)
+        {
+
+            if (conn == null)
+                conn = new SqlConnection(strConn);
+            adapter = new SqlDataAdapter("Select * from SanPham", conn);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
+            ds = new DataSet();
+            adapter.Fill(ds,"SanPham");
+            gvSanPham.DataSource = ds.Tables["SanPham"];
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            DataRow row = ds.Tables["SanPham"].NewRow();
+            row["Ma"] = txtMa.Text;
+            row["Ten"] = txtTen.Text;
+            row["DonGia"] = txtGia.Text;
+            row["MaDanhMuc"] = txtMadm.Text;
+
+            ds.Tables["SanPham"].Rows.Add(row);
+
+            int ret = adapter.Update(ds.Tables["SanPham"]);
+            if (ret > 0)
+            {
+                btnNhapDuLieu.PerformClick();
+            }
+            else
+                MessageBox.Show("Them moi that bai");
+        }
+        int vt = -1;
+        private void gvSanPham_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            vt = e.RowIndex;
+            if(vt != -1)
+            {
+                DataRow row = ds.Tables["SanPham"].Rows[vt];
+                txtMa.Text = row["Ma"] + "";
+                txtTen.Text = row["Ten"] + "";
+                txtGia.Text = row["DonGia"] + "";
+                txtMadm.Text = row["MaDanhMuc"] + "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (vt != -1)
+            {
+                DataRow row = ds.Tables["SanPham"].Rows[vt];
+                row.BeginEdit();
+                row["Ma"] = txtMa.Text;
+                row["Ten"] = txtTen.Text;
+                row["DonGia"] = txtGia.Text;
+                row["MaDanhMuc"] = txtMadm.Text;
+                row.EndEdit();
+
+                int ret = adapter.Update(ds.Tables["SanPham"]);
+                if (ret > 0)
+                {
+                    btnNhapDuLieu.PerformClick();
+                }
+            }
+        }
+    }
+}
